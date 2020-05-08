b7cc7cdafc86d1b313bb332ee9073ead5b1956f9
Mike Gretton <mikegretton@gmail.com>
2020-05-08 02:28:47 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,16 @@
 //define functions here
+function getIt() {
+  $('p').on('click').
+}
+function frameIt() {
+  $('img').on('load').
+}
+function pressIt() {
+  $('#typing').on('keydown').
+}
+function submitIt() {
+  $('form').on('submit').
+}
 
 $(document).ready(function(){
 
